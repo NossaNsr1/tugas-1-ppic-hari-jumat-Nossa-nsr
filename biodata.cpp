4945e6746211d2277998b710ed5fad9883e561bd
NossaNsr1 <nossanovianty01@gmail.com>
2024-10-02 19:36:32 +0700

    Add files via upload

---
+++ biodata.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	printf("Nama : Nossa Noviayanty Sofia Ramdani");
+	printf("NIM : 2441035");
+	printf("Jurusan : Teknik Informatika");
+	return 0;
+}
---
+++ hitungan.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int panjang, lebar, luaspersegipanjang;
+	
+	panjang=10;
+	lebar=7;
+	
+	luaspersegipanjang=panjang*lebar;
+	printf ("kd", luaspersegipanjang);
+	
+	return 0;
+}
